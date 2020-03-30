e0bae1fc6303f4c2d069386c3f1951fed93fd039
sabeurios <chikhaoui.saber@gmail.com>
2020-03-30 14:41:56 +0100

    second-commit

--- src/components/brands/brand.js
+++ src/components/brands/brand.js
@@ -2,9 +2,9 @@ import React from "react"
 
 const Brand = (props) =>{
     return  <a href="#" target="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="200" height="32" viewBox="0 0 200 32">
-                    <path d={props.path}></path>
-                </svg>
+                <div>                    
+                {props.path.map(e=>( <svg width="200" height="32" viewBox="0 0 200 32"><path d={e}></path></svg>))}
+                </div>
             </a>
 }
 export default Brand
--- src/components/cards/main-cards.js
+++ src/components/cards/main-cards.js
@@ -1,20 +1,27 @@
 import React from "react"
 import Card from "./card"
 
+let cardList=[ { 
+                  title1: "For patients",
+                  title2: "Find a doctor, book a visit and solve any health-related doubt",
+                  image:"https://www.docplanner.com/img/screen-marketplace@2x.png",
+                  showOption: true,
+                  color: "#00CCB1",
+                  margin:""
+            },
+            { 
+                  title1: "For doctors",
+                  title2: "Save time managing visits and cut no-shows by half" ,
+                  image: "https://www.docplanner.com/img/screen-saas@2x.png",
+                  showOption: false ,
+                  color: "#3D83DF",
+                  margin:"1px"
+                  }
+            ]
+
 function Cards (){
-    return <div class="cards marged">
-                <Card h2={"For patients"}
-                      h1={"Find a doctor, book a visit and solve any health-related doubt"}
-                      image={"https://www.docplanner.com/img/screen-marketplace@2x.png"}
-                      showOption={true}
-                      color={"#00CCB1"}/>
-                      
-                <Card h2={"For doctors"}
-                      h1={"Save time managing visits and cut no-shows by half"}
-                      image={"https://www.docplanner.com/img/screen-saas@2x.png"}
-                      showOption={false}
-                      color={"#3D83DF"}
-                      margin={"1px"}/>
+    return <div className="cards marged">
+                <Card infoCard={cardList} />
            </div>
 }
 export default Cards
